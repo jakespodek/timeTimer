29f2f99c74dc09faae6c0d2b6f8e72514a1de759
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2025-02-16 11:10:50 -0500

    commit

---
+++ src/components/StopWatch.tsx
@@ -0,0 +1,13 @@
+import formatTime from '../utils/formatTime';
+
+interface stopWatchProps {
+    time: number;
+}
+
+const StopWatch = ({time} : stopWatchProps) => {
+    return(
+        <p data-testid='stopwatch' className='timeDisplay digital'>{formatTime(time)}</p>
+    )
+}
+
+export default StopWatch;
---
+++ src/components/StopWatchButton.tsx
@@ -0,0 +1,36 @@
+interface buttonProps {
+    name: string;
+    stopwatchState: string;
+    handleButtonClick: () => void;
+}
+
+const StopWatchButton = ({ name, stopwatchState, handleButtonClick }: buttonProps) => {
+    // certain buttons should be disabled depending on the stopwatch state
+    // disabled button conditions:
+        // disable all buttons except the start button when the stopwatch has been reset (default)
+        // disable only the start button when the stopwatch is running
+        // disable only the stop button when the stopwatch is stopped/paused
+    if (
+        (stopwatchState === 'reset' && name !== 'start')
+        || (stopwatchState === 'started' && name === 'start')
+        || (stopwatchState === 'stopped' && name === 'stop')
+    ) {
+        return (
+            <button
+                data-testid={`${name}Button`} 
+                className='digital' 
+                onClick={handleButtonClick} 
+                disabled>{name}
+            </button>
+        )
+    // if the none of the above conditions are met, render a normal/non-disabled button
+    } else return (
+        <button 
+            data-testid={`${name}Button`} 
+            className='digital' 
+            onClick={handleButtonClick}>{name}
+        </button>
+    )
+}
+
+export default StopWatchButton;
